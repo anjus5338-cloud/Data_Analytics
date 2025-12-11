405e72fe4952e506f847b5ff14ced50e4f88d477
Anjushekhawat1 <anjus5338@gmail.com>
2025-12-11 16:54:30 +0530

    day_2

---
+++ 02.py
@@ -0,0 +1,73 @@
+# Integer Number
+a=10
+print(a)
+
+# Type
+print(type(a))
+
+# Float Number
+b = 10.5
+print(b)
+
+# Type
+print(type(b))
+
+# String Value
+c = "Hello"
+print(c)
+
+# Type
+print(type(c))
+
+# Complex Number
+d = 2 + 4j
+print(d)
+
+# Type
+print(type(d))
+
+# Variable
+x,y,z = 10,20,30
+print(x,y,z)
+
+# Type of variable
+print(type(x))
+
+# List
+l=[10,20,30,40,50]
+print(l)
+
+# Type of list
+print(type(l))
+
+# Tuple
+tuple = (1,2,3,4,5)
+print(tuple)
+
+# Type of tuple
+print(type(tuple))
+
+# Range
+r = range(5)
+print(list(r))
+
+# Type of range
+print(type(r))
+
+# Dictionary
+my_dict ={ "Name" : "Anju","Surname" : "Shekhawat"}
+print(my_dict)
+
+# Type of dictionary
+print(type(my_dict))
+
+# Set
+s = {1,2,3,4,5}
+print(s)
+
+# Type of set
+print(type(s))
+
+# Boolean
+x = True
+print(type(x))
