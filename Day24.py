0da21693ebfcca0652883e7b9bbecde72ce4323e
Anjushekhawat1 <anjus5338@gmail.com>
2026-01-03 12:50:02 +0530

    First commit

---
+++ Day24.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([0,2,4,6])
+y = np.array([8,10,12,14])
+
+#markers:
+plt.plot(x,y,marker = 'o')
+plt.show()
+
+#star
+'''plt.plot(x,y,marker = '*')
+plt.show()
+
+#point
+plt.plot(x,y,marker = '.')
+plt.show()
+
+#pixel
+plt.plot(x,y,marker = ",")
+plt.show()
+
+#cross
+plt.plot(x,y,marker = 'x')
+plt.show()
+
+#x filled
+plt.plot(x,y,marker = 'X')
+plt.show()
+
+#plus
+plt.plot(x,y,marker = "+")
+plt.show()
+
+#plus filled
+plt.plot(x,y,marker = 'P')
+plt.show()
+
+#Hexagon
+plt.plot(x,y, marker = 'H')
+plt.show()
+
+# Hexagon
+plt.plot(x,y, marker = 'h')
+plt.show()
+
+#Square
+plt.plot(x,y, marker = 's')
+plt.show()
+
+#Dimond
+plt.plot(x,y, marker = 'd')
+plt.show()
+
+#Dimond Filled
+plt.plot(x,y, marker = 'D')
+plt.show()
+
+#pentagon
+plt.plot(x,y, marker = 'P')
+plt.show()
+
+
+#Triangle up
+plt.plot(x,y, marker = '^')
+plt.show()
+
+#Triangle down
+plt.plot(x,y, marker = 'v')
+plt.show()
+
+#Triangle left
+plt.plot(x,y, marker = '<')
+plt.show()
+
+#Triangle right
+plt.plot(x,y, marker = '>')
+plt.show()
+
+#Triangle Down
+plt.plot(x,y, marker = '1')
+plt.show()
+
+#Triangle Up
+plt.plot(x,y, marker = '2')
+plt.show()
+
+#Triangle left
+plt.plot(x,y, marker = '3')
+plt.show()
+
+#Triangle right
+plt.plot(x,y, marker = '4')
+plt.show()
+
+# vartical line
+plt.plot(x,y, marker = '|')
+plt.show()'''
+
+#Format string
+plt.plot(x,y, 'o:y')
+plt.show()
+
+#solid line
+plt.plot(x,y, '*-r')
+plt.show()
+
+#dashed line
+plt.plot(x,y, '.--b')
+plt.show()
+
+#dashed dotted
+plt.plot(x,y, '+-.g')
+plt.show()
+
+#Increasing marker size
+plt.plot(x,y, marker = '*', ms = 20)
+plt.show()
+
+# marker edge color
+plt.plot(x,y, marker = '*', ms = 20, mec = 'r')
+plt.show()
+
+#marker face color
+plt.plot(x,y, marker = '*', ms = 20, mec = 'r', mfc = 'b')
+plt.show()
+
+#line widht
+plt.plot(x,y, '.--b', lw = 2)
+plt.show()
+
+# line style 
+
+plt.plot(x,y,ls = '--')
+plt.show()
+
+
+plt.plot(x,y, ls = '-.')
+plt.show()
+
+#Line color 
+plt.plot(x,y, ls = '--', color = 'r')
+plt.show()
